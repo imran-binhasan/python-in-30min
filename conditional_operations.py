e348deef9aac5c0a2fff3f31a68a84f5d582f019
imran-binhasan <imranbinhasan.work@gmail.com>
2025-06-23 09:34:30 +0600

    add conditional_operations

--- conditional_operations.py
+++ conditional_operations.py
@@ -1,4 +1,18 @@
-x = 'hello'
-y = 3
+# == strict equality
+# != strict inequality
+# <= less than or equal to
+# >= greater than or equal to
+# < strictly less than
+# > strictly greater than
 
-print( x * y)
+
+print ('a' > 'Z') #uses ASCII value for comparison
+print(ord('Z')) 
+print(True or False) #evaluates to True
+
+#order of execution: not → and → or
+print(True or False and not False)
+
+#not False → True
+#False and True → False
+#True or False → True
