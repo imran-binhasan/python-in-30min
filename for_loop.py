bd3b655d1d1453343e591aa3ef277d8e0fd9630a
imran-binhasan <imranbinhasan.work@gmail.com>
2025-06-23 10:40:25 +0600

    add while and for loop

---
+++ for_loop.py
@@ -0,0 +1,32 @@
+# stop , start, step
+# for i in range(1, 10, 3):
+#     print(i)
+# for i in range(10, 1, -3):
+#     print(i)
+    
+# for i in range(-10, -1, -1):
+#     print(i)
+# range(start, stop, step) works while going toward stop in the direction of step
+
+# Since you're stepping backward (-1), you need start > stop
+
+# stop, start
+# for i in range(1, 5):
+#     print(i) 
+    
+
+#stop
+# for i in range():
+#     print(i)
+    
+    
+    
+# loop in list
+x = [1, 2, 3, 4, 5]
+
+for i in range(len(x)):
+    print(x[i])
+    
+    
+for i, element in enumerate(x):
+    print(i, element)
